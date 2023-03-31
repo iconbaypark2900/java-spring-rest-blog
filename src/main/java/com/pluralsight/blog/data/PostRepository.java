7b02168e1e661ec9540172b5f2f0a9c4c12588a8
iconbaypark2900 <jonaston015@gmail.com>
2023-03-30 20:42:57 -0400

    creating the PostRepository

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -4,6 +4,7 @@ import com.pluralsight.blog.model.Author;
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.ArrayList;
 import java.util.List;
